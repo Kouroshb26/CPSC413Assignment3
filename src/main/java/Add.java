a01bc5471c043ffabb1f13186d4700224bdfe3d8
Kourosh Banaeianzadeh <kourosh@synopsys.com>
2019-11-06 19:25:07 -0700

    Implemented lookups
    4000 characters in 1 second

--- src/main/java/Add.java
+++ src/main/java/Add.java
@@ -3,23 +3,34 @@
  * @since 2019-11-06
  */
 public class Add {
-    private static char[][] additionRule = new char[][] {
-        {'b','c','d','b'},
-        {'b','b','c','a'},
-        {'a','a','c','b'},
-        {'b','c','d','c'}};
+    private static char[][] additionRule = new char[][]{
+        {'b', 'c', 'd', 'b'},
+        {'b', 'b', 'c', 'a'},
+        {'a', 'a', 'c', 'b'},
+        {'b', 'c', 'd', 'c'}};
 
+    private static Encoding[][] additionLookup = new Encoding[16][16];
 
-    public static Encoding add(Encoding first, Encoding second) {
-        if(first.isFull() && second.isFull()){
-            return first;
+    static {
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
+               additionLookup[i][j] = addSlow(new Encoding(i),new Encoding(j));
+            }
         }
+    }
+
+    private static Encoding addSlow(Encoding first, Encoding second) {
         Encoding answer = new Encoding();
-        for (Integer i : first.indeces()) {
-            for (Integer j : second.indeces()) {
+        for (Integer i : first.indices()) {
+            for (Integer j : second.indices()) {
                 answer.addCharacter(additionRule[i][j]);
             }
         }
         return answer;
     }
+
+    public static Encoding add(Encoding first, Encoding second){
+        return additionLookup[first.getEncoding()][second.getEncoding()];
+    }
+
 }
--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,39 +1,62 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
+import java.util.Map;
 
 /**
  * @author kourosh
  * @since 2019-11-06
  */
 public class Encoding {
-    private Boolean[] encoding = new Boolean[4];
-    private boolean isFull = false;
+    private int encoding = 0;
+    private static Map<Integer, String> encodingToString = new HashMap<>();
+
+    static {
+        //setup
+        for (int i = 0; i < 16; i++) {
+            StringBuilder builder = new StringBuilder("{");
+            if ((i & 0x1) == 1) {
+                builder.append("a,");
+            }
+            if ((i & 0x2) == 2) {
+                builder.append("b,");
+            }
+            if ((i & 0x4) == 4) {
+                builder.append("c,");
+            }
+            if ((i & 0x8) == 8) {
+                builder.append("d,");
+            }
+            builder.deleteCharAt(builder.length() - 1);
+            builder.append("}");
+            encodingToString.put(i, builder.toString());
+        }
+    }
+
+    public Encoding() {
+    }
+
+    public Encoding(int encoding) {
+        if (encoding > 15) {
+            throw new RuntimeException("Encoding too big");
+        }
+        this.encoding = encoding;
+    }
 
     private int characterToIndex(char c) {
         return c - 'a';
     }
 
-    public Encoding() {
-        Arrays.fill(encoding, Boolean.FALSE);
-    }
-
     @Override
     public String toString() {
-        return "{" + IntStream.range(0, encoding.length)
-            .filter(index -> encoding[index])
-            .mapToObj(index -> Character.toString((char) (index + 'a')))
-            .collect(Collectors.joining(",")) + "}";
+        return encodingToString.get(encoding);
     }
 
-    public List<Integer> indeces() {
+    public List<Integer> indices() {
         ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < encoding.length; i++) {
-            if (encoding[i]) {
+
+        for (int i = 0; i < 4; i++) {
+            if ((encoding & (0x1 << i)) == (0x1 << i)) {
                 list.add(i);
             }
         }
@@ -41,31 +64,26 @@ public class Encoding {
     }
 
     public void addCharacter(char c) {
-        encoding[characterToIndex(c)] = true;
+        encoding = encoding | (0x1 << characterToIndex(c));
     }
 
     public boolean isFull() {
-        if (isFull) {
-            return isFull;
+        if (encoding == 15) {
+            return true;
         } else {
-            for (int i = 0; i < encoding.length; i++) {
-                if (!encoding[i]) {
-                    isFull = false;
-                    return isFull;
-                }
-            }
+            return false;
         }
-        isFull = true;
-        return isFull;
     }
 
     public void addEncoding(Encoding encode) {
-        for (Integer i : encode.indeces()) {
-            encoding[i] = true;
-        }
+        encoding = encoding | encode.encoding;
+    }
+
+    public int getEncoding(){
+        return encoding;
     }
 
     public boolean hasC() {
-        return encoding[2];
+        return (encoding >> 2 & 0x1) == 0x1;
     }
 }
